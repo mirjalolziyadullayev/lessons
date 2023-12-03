eddd17d982b0f84cdd154b404b1596ee7f4b60bb
Mirjalol Ziyadullayev <121923945+mirjalolziyadullayev@users.noreply.github.com>
2023-12-03 09:57:58 +0500

    last commit

---
+++ Interface/Program.cs
@@ -0,0 +1,14 @@
+﻿using AnimalSound;
+using AnimalSound.Interfaces;
+
+IAnimal Animals = new Animals();
+IAnimal Dog = new Dog();
+IAnimal Goat = new Goat();
+IAnimal Cat = new Cat();
+
+
+Animals.Sound();
+Dog.Sound();
+Goat.Sound();
+Cat.Sound();
+
---
+++ Interface/goat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using AnimalSound.Interfaces;
+
+namespace AnimalSound
+{
+    public class Goat : IAnimal
+    {
+        public void Sound()
+        {
+            Console.WriteLine("Mheeeeeh");
+        }
+    }
+}
+
