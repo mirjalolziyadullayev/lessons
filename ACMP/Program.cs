8b9c5d8a076507688ed2cbf08a813c39d5c91568
Mirjalol Ziyadullayev <121923945+mirjalolziyadullayev@users.noreply.github.com>
2023-12-12 22:19:02 +0500

    last

---
+++ ACMP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace aplub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string son = Console.ReadLine();
+
+            int a = int.Parse(son.Split()[0]);
+            int b = int.Parse(son.Split()[1]);
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
