9136e30f72b313ea9b1adc2f0e84b8f404b8be59
Mirjalol Ziyadullayev <121923945+mirjalolziyadullayev@users.noreply.github.com>
2023-12-19 20:19:17 +0500

    something

--- Task Scheduler/ConsoleUI/SubMenu/UserMenu.cs
+++ Task Scheduler/ConsoleUI/SubMenu/UserMenu.cs
@@ -22,9 +22,9 @@ internal class UserMenu
 
             Console.WriteLine(" 1. Create");
             Console.WriteLine(" 2. Update");
-            Console.WriteLine(" 2. Delete");
-            Console.WriteLine(" 2. Get all");
-            Console.WriteLine(" 3. Exit...\n");
+            Console.WriteLine(" 3. Delete");
+            Console.WriteLine(" 4. Get all\n");
+            Console.WriteLine(" 5. Exit...\n");
 
             Console.WriteLine("----------------------");
             Console.Write(" Enter Your Choice: ");
@@ -33,7 +33,7 @@ internal class UserMenu
             switch (choice)
             {
                 case "1":
-                    CreatedUser:
+                    CreateUser:
                     UserModel CreateUser = new UserModel();
 
                     Console.Write("Enter your Firstname: ");
@@ -57,13 +57,60 @@ internal class UserMenu
                         Console.WriteLine(" This user is not found. Press any key to re-enter");
                         Console.WriteLine("------------------------------------------------------\n");
                         Console.ReadLine();
-                        goto CreatedUser;
+                        goto CreateUser;
                     }
                     break;
                 case "2":
-                    taskMenu.Display();
+                UpdateUser:
+                    UserModel UpdateUser = new UserModel();
+                    Console.Write("Enter your ID: ");
+                    UpdateUser.Id = int.Parse(Console.ReadLine());
+                    Console.Write("Enter your Firstname: ");
+                    UpdateUser.Firstname = Console.ReadLine();
+                    Console.Write("Enter your Lastname: ");
+                    UpdateUser.Lastname = Console.ReadLine();
+
+                    UserModel UpdatedUser = _userService.Update(UpdateUser);
+                    if (UpdatedUser != null)
+                    {
+                        Console.WriteLine("----------------------------------------");
+                        Console.WriteLine($" User ID: {UpdatedUser.Id}");
+                        Console.WriteLine($" User FIrstname: {UpdatedUser.Firstname}");
+                        Console.WriteLine($" User Lastname: {UpdatedUser.Lastname}");
+                        Console.WriteLine("----------------------------------------\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("------------------------------------------------------");
+                        Console.WriteLine(" This user is not found. Press any key to re-enter");
+                        Console.WriteLine("------------------------------------------------------\n");
+                        Console.ReadLine();
+                        goto UpdateUser;
+                    }
                     break;
                 case "3":
+                    List<UserModel> AllUsers = _userService.GetAll();
+                    if (AllUsers != null)
+                    {
+                        foreach (UserModel user in AllUsers)
+                        {
+                            Console.WriteLine("----------------------------------------");
+                            Console.WriteLine($" User ID: {user.Id}");
+                            Console.WriteLine($" User FIrstname: {user.Firstname}");
+                            Console.WriteLine($" User Lastname: {user.Lastname}");
+                            Console.WriteLine("----------------------------------------\n");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("------------------------------------------------------");
+                        Console.WriteLine(" User list is empty. Press any key to go back");
+                        Console.WriteLine("------------------------------------------------------\n");
+                        Console.ReadLine();
+                        continue;
+                    }
+                    break;
+                case "5":
                     Console.Write("Exit...");
                     Console.ReadLine();
                     loop = false;
